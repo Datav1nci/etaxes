bbabc34719fc94a81c3faa3719e4d91f8a16b237
Datav1nci <jestais4@gmail.com>
2025-02-22 17:12:29 -0500

    contact us chnaged to nous joindre

--- src/utils/header.js
+++ src/utils/header.js
@@ -104,7 +104,7 @@ function ResponsiveAppBar() {
               sx={{ mx: 1, border: "1px solid black", color: "black", fontSize: { xs: "12px", sm: "14px" } }}
               onClick={handleOpenContact}
             >
-              Contact Us
+              Nous oindre
             </Button>
           </Box>
 
